4895666ad5810c685d91429e98d94d7f279f8513
jim WATKYNS <jwatkyn@c1r3s6.wethinkcode.co.za>
2018-06-06 12:00:01 +0200

    Final push

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jwatkyn <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/01 14:20:59 by jwatkyn           #+#    #+#             */
-/*   Updated: 2018/06/01 15:36:37 by jwatkyn          ###   ########.fr       */
+/*   Updated: 2018/06/06 11:40:56 by jwatkyn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,23 +35,26 @@ static int	ft_gnl(char buf[BUFF_SIZE + 1], char **l, int *r, const int fd)
 		*r = read(fd, &buf[0], BUFF_SIZE);
 		buf[*r] = 0;
 	}
-	return (0);
+	if (i >= 0)
+		return (1);
+	else
+		return (0);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	static char		buf[1000][BUFF_SIZE + 1];
+	static char		buf[1000000][BUFF_SIZE + 1];
 	int				ret;
 	int				i;
 
 	if (!line || fd < 0)
 		return (-1);
-	if (!(*line = ft_strnew(52001)))
+	if (!(*line = (char *)malloc(5200000)))
 		return (-1);
 	ret = ft_strlen(&buf[fd][0]);
 	if (!ret)
 		ret = read(fd, &buf[fd][0], BUFF_SIZE);
-	buf[fd][ret + 1] = 0;
+	buf[fd][ret] = 0;
 	i = ft_gnl((char*)(buf[fd]), &*line, &ret, fd);
 	if (ft_strlen(*line) > 0 || i == 1)
 		return (1);
--- get_next_line.h
+++ get_next_line.h
@@ -6,26 +6,16 @@
 /*   By: jwatkyn <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 08:39:57 by jwatkyn           #+#    #+#             */
-/*   Updated: 2018/05/30 15:51:57 by jwatkyn          ###   ########.fr       */
+/*   Updated: 2018/06/05 13:55:51 by jwatkyn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <unistd.h>
 # include "libft.h"
 
-# define BUFF_SIZE 34
+# define BUFF_SIZE 100
 
 int				get_next_line(const int fd, char **line);
-
-typedef struct	s_line
-{
-	int		fd;
-	char	*line;
-	size_t	len;
-	size_t	linepos;
-	int		ret;
-}				t_line;
 
 #endif
